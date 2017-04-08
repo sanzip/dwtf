8065ed14f8dd3c2bf992ab25552e11654450d4ae
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-09 02:48:58 +0545

    up

--- db_connection.php
+++ db_connection.php
@@ -1,31 +1,30 @@
-<?php
-$conn = pg_connect("dbname=d2nip5a2dq6nrd");
+
+<!-- $conn = pg_connect("dbname=d2nip5a2dq6nrd");
 
 
 if (!$conn) {
 
 
-    die("Error in connection: " . pg_last_error());
+     die("Error in connection: " . pg_last_error());
 
 
-}
-// $servername = "localhost";
-// $username = "root";
-// $password = "root";
-// $database= "dwtfp";
-// // Create connection
-// $conn = mysqli_connect($servername, $username, $password, $database);
+ }
+ $servername = "localhost";
+ $username = "root";
+ $password = "root";
+ $database= "dwtfp";
+ Create connection
+ $conn = mysqli_connect($servername, $username, $password, $database);
 
-// // Check connection
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-?>
+ Check connection
+ if (!$conn) {
+     die("Connection failed: " . mysqli_connect_error());
+ } -->
 
-<!-- $dbname="d2nip5a2dq6nrd";
-host= "ec2-54-197-232-155.compute-1.amazonaws.com";
+<?php
+$dbname="d2nip5a2dq6nrd";
+$host= "ec2-54-197-232-155.compute-1.amazonaws.com";
 $username="qehavbestclndn";
 $password="a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e";
 $conn = new PDO("pgsql:dbname=$dbname;host=$host", $username, $password ); 
-
- -->
+?>
