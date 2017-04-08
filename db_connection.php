2060be7bccf4d695e0462416db45dabf73912402
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-09 01:22:32 +0545

    file changed

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$database= "dwtfp";
+$servername = "ec2-54-197-232-155.compute-1.amazonaws.com";
+$username = "qehavbestclndn";
+$password = "a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e";
+$database= "d2nip5a2dq6nrd";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
