124cc21af691c386b73ca7b0a99d969bd4241747
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-09 02:25:36 +0545

    databbase_updte

--- db_connection.php
+++ db_connection.php
@@ -1,14 +1,21 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$database= "dwtfp";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+// $servername = "localhost";
+// $username = "root";
+// $password = "root";
+// $database= "dwtfp";
+// // Create connection
+// $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+// // Check connection
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+<?php 
+$dbname="d2nip5a2dq6nrd";
+$host= "ec2-54-197-232-155.compute-1.amazonaws.com";
+$username="qehavbestclndn";
+$password="a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e";
+$conn = new PDO("pgsql:dbname=$dbname;host=$host", $username, $password ); 
+?> 
 
 ?>
