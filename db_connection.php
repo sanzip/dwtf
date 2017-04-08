2eba0173253ceda88ecce03a480435f66968f3b0
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-09 02:13:26 +0545

    update

--- db_connection.php
+++ db_connection.php
@@ -1,23 +1,14 @@
 <?php
-// $dsn = "pgsql:"
-//     . "ec2-54-197-232-155.compute-1.amazonaws.com;"
-//     . "dbname=d2nip5a2dq6nrd;"
-//     . "user=qehavbestclndn;"
-//     . "port=5432;"
-//     . "sslmode=require;"
-//     . "password=a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e";
-
-// $db = new PDO($dsn);
-$servername = "ec2-54-197-232-155.compute-1.amazonaws.com";
-$username = "qehavbestclndn";
-$password = "a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e";
-$database= "d2nip5a2dq6nrd";
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$database= "dwtfp";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
 if (!$conn) {
-    die("Connection fail: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 
 ?>
