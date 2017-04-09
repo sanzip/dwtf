5602162cb72b82781b0dab69405996e9f447195b
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-10 00:12:18 +0545

    query updated

--- create_user_action.php
+++ create_user_action.php
@@ -21,8 +21,9 @@ if(isset($_POST['btn_create']) && ($_SERVER['REQUEST_METHOD']== "POST"))
   $fellowship_place=$_POST['fplace'];
 	$photo=file_get_contents($photo);
 	$photo=base64_encode($photo);
-	$qry="insert into user values('$userid','$username','$password','$email','$fellowship_place','$batch','$number','$photo')";
-   $result=pg_query($conn,$qry);
+  $qry = "INSERT INTO user() VALUES('" . $userid . "', '" . $username . "', '" . $password . "','" . $email . "','" . $fellowship_place . "','" . $batch . "','" . $number . "','" . $photo . "')";
+	// $qry="insert into user values('$userid','$username','$password','$email','$fellowship_place','$batch','$number','$photo')";
+   $result=pg_query($conn, $qry);
    
    if($result)
    {
