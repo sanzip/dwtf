d28dfde9e89bd1f9243bebec0b84a9c8e02434ef
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-09 23:46:54 +0545

    conection updated

--- db_connection.php
+++ db_connection.php
@@ -20,11 +20,20 @@ if (!$conn) {
  if (!$conn) {
      die("Connection failed: " . mysqli_connect_error());
  } -->
-
 <?php
-$dbname="d2nip5a2dq6nrd";
+    $conn = pg_connect("host=ec2-54-197-232-155.compute-1.amazonaws.com dbname=d2nip5a2dq6nrd user=qehavbestclndn password=a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e");
+if (!$conn) {
+
+
+     die("Error in connection: " . pg_last_error());
+
+
+ }
+?>
+
+<!-- $dbname="d2nip5a2dq6nrd";
 $host= "ec2-54-197-232-155.compute-1.amazonaws.com";
 $username="qehavbestclndn";
 $password="a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e";
 $conn = new PDO("pgsql:dbname=$dbname;host=$host", $username, $password ); 
-?>
+ -->
