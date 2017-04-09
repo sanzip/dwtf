e94c99e9f16626210cd62fbe2def93565f83ad15
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-10 04:51:11 +0545

    service test

--- login_service.php
+++ login_service.php
@@ -2,13 +2,13 @@
 
 include "DbConnection.php";
 
-$username=$_POST["username"];
-$password=$_POST["password"];
-$query="Select * from users where email='$username' AND password='$password'";
+$username=$_GET["username"];
+$password=$_GET["password"];
+$query="Select username,email,batch,fellowship_place,number from users where email='$username' AND password='$password'";
 $result = pg_query($conn, $query);
 $row = pg_fetch_assoc($result);
 if($row){
-    $data = Array("login" => true,"username"=>$row["username"],"email"=>$row["email"],"place"=>$row["fellowship_place"],"Batch"=>$row["batch"],"contact"=>$row["number"],"image"=>$row["image"]);
+    $data = Array("login" => true,"username"=>$row["username"],"email"=>$row["email"],"place"=>$row["fellowship_place"],"Batch"=>$row["batch"],"contact"=>$row["number"]);
     echo json_encode($data);
 }
 else
