cf7f6679f130d282534cd984ef5fb4cd5a035888
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-10 02:40:43 +0545

    update

--- homepage.php
+++ homepage.php
@@ -9,12 +9,7 @@
 <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
-   <?php
-  session_start();
-if(!isset($_SESSION['login_user'])){
-   header("location:index.php");
-}
-  ?>
+  
     <header class="main__header">
   <div class="container">
     <nav class="navbar navbar-default" role="navigation"> 
