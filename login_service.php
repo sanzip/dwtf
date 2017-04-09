5be5b7bef853b0e03c915ef7740ed5c4ef03f9f0
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-10 01:09:15 +0545

    update

--- login_service.php
+++ login_service.php
@@ -5,7 +5,7 @@ include "DbConnection.php";
 $username=$_GET["username"];
 $password=$_GET["password"];
 $query="Select *from login where email='$username' AND password='$password'";
-$result = $conn -> query($query);
+$result = pg_query($conn, $query);
 $row = pg_fetch_assoc($result);
 if($row){
     $data = Array("login" => true,"username"=>$row["username"],"email"=>$row["email"],"place"=>$row["fellowship_place"],"Batch"=>$row["batch"],"contact"=>$row["number"],"image"=>$row["image"]);
