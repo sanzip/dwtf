5a248d2aa6ca8b584f2853df8746651c2cdf59b3
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-09 23:55:13 +0545

    sql update

--- create_user_action.php
+++ create_user_action.php
@@ -21,7 +21,7 @@ if(isset($_POST['btn_create']) && ($_SERVER['REQUEST_METHOD']== "POST"))
   $fellowship_place=$_POST['fplace'];
 	$photo=file_get_contents($photo);
 	$photo=base64_encode($photo);
-	$qry="insert into user(user_id,username,password,email,fellowship_place,batch,number,image) values('$userid','$username','$password','$email','$fellowship_place','$batch','$number','$photo')";
+	$qry="insert into user values('$userid','$username','$password','$email','$fellowship_place','$batch','$number','$photo')";
    $result=pg_query($conn,$qry);
    
    if($result)
