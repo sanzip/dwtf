2e4237ed2d3cd7a9a4eda356dd0cd95fd0b39230
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-11 01:14:11 +0545

    updated

--- login_service.php
+++ login_service.php
@@ -5,7 +5,7 @@
 $username=$_POST["username"];
 $password=$_POST["password"];
 $conn = pg_connect("host=ec2-54-197-232-155.compute-1.amazonaws.com dbname=d2nip5a2dq6nrd user=qehavbestclndn password=a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e");
-$query="select user_id,username,email,fellowship_place,contact from users where email='$username' AND password='$password'";
+$query="select user_id,username,email,fellowship_place,number from users where email='$username' AND password='$password'";
 $result = pg_query($query);
 $row = pg_fetch_assoc($result);
 if($row){
