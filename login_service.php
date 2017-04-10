2a6b712049cd1406cc3ebe7eb7df18d211ec76d1
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-10 23:14:57 +0545

    connection remake

--- login_service.php
+++ login_service.php
@@ -4,8 +4,8 @@
 
 $username=$_POST["username"];
 $password=$_POST["password"];
-$query="select username,email,batch,fellowship_place,number from users where email='$username' AND password='$password'";
 $conn = pg_connect("host=ec2-54-197-232-155.compute-1.amazonaws.com dbname=d2nip5a2dq6nrd user=qehavbestclndn password=a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e");
+$query="select username,email,batch,fellowship_place,number from users where email='$username' AND password='$password'";
 $result = pg_query($query);
 $row = pg_fetch_assoc($result);
 if($row){
