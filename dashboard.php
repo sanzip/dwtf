6d368592ce29d60c01d7918e0d69e6df34f6f0db
unknown <smainali@dwnpcpu195-pc.dwnp.deerwalk.com>
2017-04-10 17:33:26 +0545

    dashboard

---
+++ dashboard.php
@@ -0,0 +1,93 @@
+<html>
+<title></title>
+<head>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+<!-- <link href='http://fonts.googleapis.com/css?family=Arvo:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800' rel='stylesheet' type='text/css'> -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/dwtf.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet">
+</head>
+<body>
+
+    <header class="main__header">
+  <div class="container">
+    <nav class="navbar navbar-default" role="navigation"> 
+      <!-- Brand and toggle get grouped for better mobile display --> 
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="navbar-header">
+        <h1 class="navbar-brand"><a >Deerwalk Teaching Fellowship(DWTF)</a></h1>
+     
+   </div>
+    <div class="menuBar">
+        <ul class="menu">
+          <li ><a > 
+    
+     
+          <li><a href="index.php">Logout</a></li>
+         <li><a  href="createuser.php" class="btn btn-primary">Create User</a></li>
+        </ul>
+      </div>
+      <!-- /.navbar-collapse --> 
+    </nav>
+  </div>
+</header>
+<br>
+<br>
+<br>
+<br>
+<div class="container">
+<div class="row text-center pad-top">
+                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.html" >
+ <i class="fa fa-circle-o-notch fa-5x"></i>
+                      <h4>Check Data</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div> 
+                 
+                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.html" >
+ <i class="fa fa-user fa-5x"></i>
+                      <h4>Register User</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div> 
+                     
+           
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.html" >
+ <i class="fa fa-lightbulb-o fa-5x"></i>
+                      <h4>New Issues</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div>
+<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">	
+                      <div class="div-square">
+                           <a href="blank.html" >
+ <i class="fa fa-users fa-5x"></i>
+                      <h4>See Users</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div>
+
+              </div>
+			  </div>
+
+<script type="text/javascript" src="js/jquery.min.js"></script> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.min.js"></script> 
+</body>
+</html>
