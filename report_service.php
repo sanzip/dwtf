e1a2f9ae93d163ae5d160c53f7e78a97554a05a3
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-11 02:27:24 +0545

    update

--- report_service.php
+++ report_service.php
@@ -8,7 +8,7 @@ $Thours=$_POST["Thours"];
 $user_id=$_POST["user_id"];
 $conn = pg_connect("host=ec2-54-197-232-155.compute-1.amazonaws.com dbname=d2nip5a2dq6nrd user=qehavbestclndn password=a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e");
 
-     $queryR="insert into reports values(1,$user_id,'$title','$story','$date',$classT,$Thours)";
+     $queryR="insert into reports values(5,$user_id,'$title','$story','$date',$classT,$Thours)";
     //$conn -> query($queryR);
      $resultC = pg_query($queryR);
    // $resultC = $conn -> query($queryR);
