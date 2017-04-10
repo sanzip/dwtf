73939a2e4dbe4f82b77fac735dae2c1dd50a4fa9
unknown <smainali@dwnpcpu195-pc.dwnp.deerwalk.com>
2017-04-10 13:52:28 +0545

    update

--- login_service.php
+++ login_service.php
@@ -1,11 +1,12 @@
 <?php
 
-include "DbConnection.php";
+//include "DbConnection.php";
 
 $username=$_GET["username"];
 $password=$_GET["password"];
 $query="select username,email,batch,fellowship_place,number from users where email='$username' AND password='$password'";
-$result = pg_query($conn, $query);
+$conn = pg_connect("host=ec2-54-197-232-155.compute-1.amazonaws.com dbname=d2nip5a2dq6nrd user=qehavbestclndn password=a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e");
+$result = pg_query($query);
 $row = pg_fetch_assoc($result);
 if($row){
     $data = Array("login" => true,"username"=>$row["username"],"email"=>$row["email"],"place"=>$row["fellowship_place"],"Batch"=>$row["batch"],"contact"=>$row["number"]);
