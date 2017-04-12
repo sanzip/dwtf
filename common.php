87a12b80a89aac4cea2c87e04122dda1aeb9e463
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-12 23:17:02 +0545

    update

--- common.php
+++ common.php
@@ -11,43 +11,11 @@ class Common {
     }
 
 
-    public function deletebanner($inputdata='')
-    {
-        $id=$inputdata['id'];
-		echo $id;
-        $rs=mysqli_query($this->conn,"delete from banner where banner_id=$id");
-		
-     $data = array();
-       
-       if($rs)
-        {
-            $data['status'] = "success";
-            
-            
-        }
-        return $data;
-}
-    public function deletegallery($inputdata='')
-    {
-        $id=$inputdata['id'];
-		echo $id;
-        $rs=mysqli_query($this->conn,"delete from galery where gallery_id=$id");
-		
-     $data = array();
-       
-       if($rs)
-        {
-            $data['status'] = "success";
-            
-            
-        }
-        return $data;
-}
     public function deleteuser($inputdata='')
     {
         $id=$inputdata['id'];
-		echo $id;
-        $rs=mysqli_query($this->conn,"delete from users where user_id=$id");
+		//echo $id;
+        $rs=pg_query($this->conn,"delete from users where user_id=$id");
 		
      $data = array();
        
