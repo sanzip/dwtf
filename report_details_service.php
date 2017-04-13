e28992dbb1c7049d40cb13877926696a4dadd513
unknown <smainali@dwnpcpu195-pc.dwnp.deerwalk.com>
2017-04-13 14:19:02 +0545

    update

--- report_details_service.php
+++ report_details_service.php
@@ -7,12 +7,11 @@ $queryR="select * from reports where user_id='$user_id'";
    // $resultC = $conn -> query($queryR);user_id,title,body,date,class_taught,hours_taught
     $row = pg_fetch_assoc($resultC);
     if($row){
-        $data = Array("save" => "Your report saved successfully",'result'=>'success');
 		 $data = Array("title"=>$row["title"],"body"=>$row["body"],"date"=>$row["date"],"class_taught"=>$row["class_taught"],"hours_taught"=>$row["hours_taught"]);
     
         echo json_encode($data);
     }else{
-        $data = Array("save" => "Error while saving your report..",'result'=>'error');
+        $data = Array("save" => "False");
         echo json_encode($data);
     }
 
