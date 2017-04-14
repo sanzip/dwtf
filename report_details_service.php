9e701cb044e15b2ae886a8119851b1458da3950a
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-15 01:23:16 +0545

    update

--- report_details_service.php
+++ report_details_service.php
@@ -3,7 +3,7 @@ $user_id=$_POST["user_id"];
 $conn = pg_connect("host=ec2-54-197-232-155.compute-1.amazonaws.com dbname=d2nip5a2dq6nrd user=qehavbestclndn password=a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e");
 $queryR="select * from reports where user_id='$user_id'";
 $resultC = pg_query($queryR);
-$row = pg_fetch_assoc($resultC)
+$row = pg_fetch_assoc($resultC);
 if($row){
     $data = Array("result"=>"success","message" => "success","title"=>$row["title"],"body"=>$row["body"],"date"=>$row["date"],"class_taught"=>$row["class_taught"],"hours_taught"=>$row["hours_taught"]);
     echo json_encode($data);
