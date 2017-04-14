fb5fb98b275f34e1da67a6bef56e6d3b69280b0f
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-15 01:56:25 +0545

    post

--- detailreport_service.php
+++ detailreport_service.php
@@ -1,5 +1,5 @@
 <?php
-$user_id=$_GET["user_id"];
+$user_id=$_POST["user_id"];
 $conn = pg_connect("host=ec2-54-197-232-155.compute-1.amazonaws.com dbname=d2nip5a2dq6nrd user=qehavbestclndn password=a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e");
 $query="select * from reports where user_id='$user_id'";
 $result = pg_query($query);
