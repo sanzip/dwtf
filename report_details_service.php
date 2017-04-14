fa1b52e3bd4aa68307bda7ec10c928aa18a4f7db
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-15 00:10:30 +0545

    update

--- report_details_service.php
+++ report_details_service.php
@@ -5,13 +5,17 @@ $queryR="select * from reports where user_id='$user_id'";
     //$conn -> query($queryR);
      $resultC = pg_query($queryR);
    // $resultC = $conn -> query($queryR);user_id,title,body,date,class_taught,hours_taught
-    $row = pg_fetch_assoc($resultC);
-    if($row){
-		 $data = Array("title"=>$row["title"],"body"=>$row["body"],"date"=>$row["date"],"class_taught"=>$row["class_taught"],"hours_taught"=>$row["hours_taught"]);
+      $data=array();
+   while( $row = pg_fetch_assoc($resultC)){
+
     
-        echo json_encode($data);
-    }else{
-        $data = Array("save" => "False");
+		 $data[] = array("result"=>"success","message" => "success","title"=>$row["title"],"body"=>$row["body"],"date"=>$row["date"],"class_taught"=>$row["class_taught"],"hours_taught"=>$row["hours_taught"]);
+    
+       
+    }
+ echo json_encode($data);
+    else{
+        $data = array("message" => "sorry! something went wrong..","result"=>"success");
         echo json_encode($data);
     }
 
