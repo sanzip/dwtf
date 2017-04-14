486ebae3f40cea5071d06b20cdcf8178fa76bd67
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-15 00:30:55 +0545

    update

--- report_details_service.php
+++ report_details_service.php
@@ -7,11 +7,13 @@ $queryR="select * from reports where user_id='$user_id'";
    // $resultC = $conn -> query($queryR);user_id,title,body,date,class_taught,hours_taught
       $row = pg_fetch_assoc($resultC))
 if($row){
-		 $data = Array("result"=>"success","message" => "success","title"=>$row["title"],"body"=>$row["body"],"date"=>$row["date"],"class_taught"=>$row["class_taught"],"hours_taught"=>$row["hours_taught"]);
-       echo json_encode($data);
+		    // $data = Array("result"=>"success","message" => "success","title"=>$row["title"],"body"=>$row["body"],"date"=>$row["date"],"class_taught"=>$row["class_taught"],"hours_taught"=>$row["hours_taught"]);
+      //   echo json_encode($data);
+  echo "yes"
     }
     else{
-        $data = Array("message" => "sorry! something went wrong..","result"=>"success");
-        echo json_encode($data);
+        // $data = Array("message" => "sorry! something went wrong..","result"=>"success");
+        // echo json_encode($data);
+      echo "no";
     }
 
