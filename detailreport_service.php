629f330e5f1409c138bf90f9e7e6a8d5e5cc242f
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-15 01:37:13 +0545

    update

--- detailreport_service.php
+++ detailreport_service.php
@@ -4,15 +4,14 @@ $conn = pg_connect("host=ec2-54-197-232-155.compute-1.amazonaws.com dbname=d2nip
 $query="select * from reports where user_id='$user_id'";
 $result = pg_query($query);
 $row = pg_fetch_assoc($result);
-$data=array();
 while($row){
     $data[] = array("result"=>"success","message" => "success","title"=>$row["title"],"body"=>$row["body"],"date"=>$row["date"],"class_taught"=>$row["class_taught"],"hours_taught"=>$row["hours_taught"]);
     
 }
 echo json_encode($data);
-else{
-  $data = Array("message" => "sorry! something went wrong..","result"=>"success");
-  echo json_encode($data);
-}
+// else{
+//   $data = Array("message" => "sorry! something went wrong..","result"=>"success");
+//   echo json_encode($data);
+// }
   
 
