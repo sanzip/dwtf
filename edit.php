f7cf78ee909f363650d3f3fbdde3cc893d7de967
sanzip <sanjeev.mainali@deerwalk.edu.np>
2017-04-17 21:33:42 +0545

    update

--- edit.php
+++ edit.php
@@ -41,6 +41,8 @@ if(isset($_POST['update']))
 <?php
 //getting id from url
 $id = $_GET['id'];
+ $conn = pg_connect("host=ec2-54-197-232-155.compute-1.amazonaws.com dbname=d2nip5a2dq6nrd user=qehavbestclndn password=a31fe85afd8c39ebb35d8467850f370272dfa359256d6b668d0a92754bb1280e");
+  
  
 //selecting data associated with this particular id
 $result = pg_query("SELECT * FROM users WHERE user_id=$id");
